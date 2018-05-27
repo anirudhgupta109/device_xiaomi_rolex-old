e5f9e07d64923cadbdfa20c7028f67a36227e179
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-05-27 10:00:43 +0000

    [2/2] rolex: Hardcode higher fps rate for certain apps
    Adapt to Haran's commit https://github.com/dev-haran/android_device_xiaomi_rolex/commit/32f9454f4ff6d4d55d5f319fbc91af0a1bb5d3e3
    
    Part [1/2] is here: https://github.com/anirudhgupta109/device_xiaomi_rolex/commit/bcff309c84b452e108424abf7e6f190ce73bb259
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -6579,13 +6579,9 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
     property_get("persist.debug.set.fixedfps", value, "0");
     fixedFpsValue = atoi(value);
 
-    if (max_fps >= 24000 && min_fps == max_fps) {
+    if (max_fps >= 30000 && min_fps == max_fps) {
         LOGH("min_fps %d same as max_fps %d, setting min_fps to 7000", min_fps, max_fps);
         min_fps = 7000;
-    }
-    if (vid_max_fps >= 24000 && vid_min_fps == vid_max_fps) {
-        LOGH("vid_min_fps %d same as vid_max_fps %d, setting vid_min_fps to 7000", vid_min_fps, vid_max_fps);
-        vid_min_fps = 7000;
     }
 
     LOGD("E minFps = %d, maxFps = %d , vid minFps = %d, vid maxFps = %d",
